cf111af98978116141fd3fa3dbc89152af6d0ec2
関口 咲季 <s-sekiguchi@i-enter.co.jp>
2019-04-18 10:54:06 +0900

    登録ボタン追加

---
+++ taskApp/Task.swift
@@ -0,0 +1,30 @@
+//
+//  Task.swift
+//  taskApp
+//
+//  Created by 関口 咲季 on 2019/04/17.
+//  Copyright © 2019 saki.sekiguchi. All rights reserved.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    @objc dynamic var id = 0
+    
+    // タイトル
+    @objc dynamic var title = ""
+    
+    // 内容
+    @objc dynamic var contents = ""
+    
+    /// 日時
+    @objc dynamic var date = Date()
+    
+    /**
+     id をプライマリーキーとして設定
+     */
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
